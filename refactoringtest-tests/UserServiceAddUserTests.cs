26e155ca6b0817b4452cfc46159dc6dece0fa138
Fundraising For Clubs <62012265+FundraisingForClubs@users.noreply.github.com>
2023-12-08 08:50:26 +0000

    Add files via upload

---
+++ refactoringtest-tests/UserServiceAddUserTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyApp
+{
+    public class UserServiceAddUserTests
+    {
+        private readonly UserService _userService;
+
+        public UserServiceAddUserTests()
+        {
+            _userService = new UserService();
+        }
+
+        [Fact]
+        public void CreateUser_All_Valid_Fields()
+        {
+            // Arrange
+            string firstName = "New";
+            string surname = "User";
+            string email = "newuser@ukcows.com";
+            DateTime dateOfBirth = DateTime.Now.AddYears(-33);
+
+            // Act
+            var User = _userService.CreateUser(firstName, surname, email, dateOfBirth);
+            
+            // Assert
+            Assert.NotNull(User);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("  ")]
+        public void CreateUser_Invalid_Firstname(string firstName) 
+        {
+            string surname = "User";
+            string email = "newuser@ukcows.com";
+            DateTime dateOfBirth = DateTime.Now.AddYears(-33);
+
+            // Act
+            var User = _userService.CreateUser(firstName, surname, email, dateOfBirth);
+
+            // Assert
+            Assert.Null(User);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("  ")]
+        public void CreateUser_Invalid_Surname(string surname)
+        {
+            string firstName = "New";
+            string email = "newuser@ukcows.com";
+            DateTime dateOfBirth = DateTime.Now.AddYears(-33);
+
+            // Act
+            var User = _userService.CreateUser(firstName, surname, email, dateOfBirth);
+
+            // Assert
+            Assert.Null(User);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("  ")]
+        [InlineData("newuser@ukcows")]
+        [InlineData("newuser.ukcows.com")]
+        public void CreateUser_Invalid_Email(string email)
+        {
+            // Arrange
+            string firstName = "New";
+            string surname = "User";
+            DateTime dateOfBirth = DateTime.Now.AddYears(-33);
+
+            // Act
+            var User = _userService.CreateUser(firstName, surname, email, dateOfBirth);
+
+            // Assert
+            Assert.Null(User);
+        }
+
+        [Fact]
+        public void CreateUser_Invalid_DateOfBirth()
+        {
+            // Arrange
+            string firstName = "New";
+            string surname = "User";
+            string email = "newuser@ukcows.com";
+            DateTime dateOfBirth = DateTime.Now.AddYears(-15);
+
+            // Act
+            var User = _userService.CreateUser(firstName, surname, email, dateOfBirth);
+
+            // Assert
+            Assert.Null(User);
+        }
+    }
+}
---
+++ refactoringtest-tests/UserServiceCreditCheckTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyApp
+{
+    public class UserServiceCreditCheckTests
+    {
+        private readonly UserService _userService;
+
+        public UserServiceCreditCheckTests()
+        {
+            _userService = new UserService();
+        }
+
+        [Theory]
+        [InlineData("VeryImportantClient")]
+        [InlineData("veryImportantclient")]
+        [InlineData("veryimportantclient")]
+        public void HasCreditLimit_Valid_ClientName_Returns_False(string clientName)
+        {
+            // Arrange
+
+            // Act
+            bool result = _userService.HasCreditLimit(clientName);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("Very Important Client")]
+        [InlineData("Importantclient")]
+        [InlineData("importantclient")]
+        [InlineData("AnoyOtherClient")]
+        public void HasCreditLimit_Valid_ClientName_Returns_True(string clientName)
+        {
+            // Arrange
+
+            // Act
+            bool result = _userService.HasCreditLimit(clientName);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetCreditLimitMultiplier_ImportantClient_Result2()
+        {
+            // Arrange
+            string clientName = "ImportantClient";
+
+            // Act
+            int result = _userService.CreditLimitMultiplier(clientName);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        public void GetCreditLimitMultiplier_Not_ImportantClient_Result1()
+        {
+            // Arrange
+            string clientName = "AnyOtherClient";
+
+            // Act
+            int result = _userService.CreditLimitMultiplier(clientName);
+
+            // Assert
+            Assert.Equal(1, result);
+
+        }
+
+
+
+    }
+}
